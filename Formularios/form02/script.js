a4e6dd7b384b28b0602529e3f27099b5c58c56e6
Mussinady Abubacar <mussinadyabubacar20@gmail.com>
2023-06-27 09:48:10 +0100

    Modificacões

--- Formularios/form02/script.js
+++ Formularios/form02/script.js
@@ -2,27 +2,77 @@ let form = document.getElementById('iform')
 let inputName = document.getElementById('inome')
 let inputEmail = document.getElementById('iemail')
 let inputpwd = document.getElementById('ipwd')
+let selectJob = document.querySelector('#ijob')
 
-form.addEventListener('submit', function (event) {
-    
+// FUNÇÃO VERIFICAR SE O E-MAIL É VALIDO COM UMA REGEX.
+function validEmail(email) {
+
+    // criar uma regex para validar o e-mail
+    const emailRegex = new RegExp (
+
+        // user12@gmail.com.br => este é o nosso padrão
+        /^[a-zA-z0-9._-]+@[a-z]+\.[a-z]{2,}$/
+    )
+
+    if (emailRegex.test(email)) {
+
+        return true
+
+    } else {
+
+        return false
+    }
+}
+
+
+// VERIFICAR SE A SENHA É VALIDA.
+function validPassword(pwd, minDigit) {
+
+    if (pwd.length >= minDigit) {
+
+        return true
+
+    } else {
+
+        return false
+    }
+} 
+
+// EVENTO DE SUBMISÃO DO FORMULÁRIO
+form.addEventListener('submit', (event) => {
+
+    // NÃO FAÇA A SUBMISÃO ATÉ QUE AS VALIDAÇÕES TERMINEM.
     event.preventDefault()
 
-    NomeValid()
+    // VERIFICAR SE O CAMPO DE NOME ESTA VAZIA OU NÃO.
+    if (inputName.value === '') {
 
-})
+        alert('POR FAVOR! VERIFIQUE O CAMPO DE NOME ESTA VAZIA.')
+        return;
+    }
 
-function NomeValid() {
-    
-    if (inputName.value === "") {
+    // VERIFICAR SE O CAMPO DE E-MAIL ESTA VAZIA OU SE O E-MAIL É VALIDO
+    if (inputEmail.value === '' || !validEmail(inputEmail.value)) {
 
-        inputName.style.border = "1px solid red"
-    } else {
-        inputName.style.border = "1px solid blue"
+        alert('POR FAVOR! VERIFIQUE O CAMPO DE E-MAIL NÃO ESTA BEM PREENCHIDA.')
+        return;
+    }
+
+    // VERIFICAR SE A SENHA É VALIDA.
+    if (!validPassword(inputpwd.value, 8)) {
+
+        alert('POR FAVOR! VERIFIQUE O CAMPO DE PASSWORD O DIGITO TEM QUE SER MAIOR OU IGUAL A 8 PALAVRAS.')
+        return;
+    }
+
+    // VERIFICAR SE A SITUAÇÃO DE TRABALHO FOI SELECIONADO OU NÃO
+    if (selectJob.value === '') {
+
+        alert('POR FAVOR! SELECIONE A SUA SITUAÇÃO DE TRABALHO NO CAMPO ABAIXO.')
+        return;
     }
 
 
-}
-
-function validEmail() {
-
-}
+    // SE TODAS AS VALIDAÕES FORMEM VERIFICADAS ENTÃO FAÇA SUBMISÃO.
+    form.submit();
+})
