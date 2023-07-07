2f423494b2a3e5752225c554956104f381e3b528
Mussinady Abubacar <mussinadyabubacar20@gmail.com>
2023-07-07 16:35:38 +0100

    mudanças

--- Dark-mode/darkmode01/script.js
+++ Dark-mode/darkmode01/script.js
@@ -1,13 +1,9 @@
 const body = document.querySelector('.body');
-const main = document.querySelector('.main')
 const swipe = document.querySelector('.swipe');
-const title = document.querySelector('.title')
 
 swipe.addEventListener('click', () => {
 
     swipe.classList.toggle('toggle')
     body.classList.toggle('dark')
-    main.classList.toggle('dark')
-    title.classList.toggle('dark')
     
 })
